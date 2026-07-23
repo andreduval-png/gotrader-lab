1489b87312a799751c24e4197b965dca8e887142
Andre Duval <andre.duval@nationofduval.org>
2026-07-23 08:09:26 -0400

    Add one-command GoTrader launcher

--- Start-GoTrader.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if not exist ".gotrader" mkdir ".gotrader"
-start "GoTrader Supervisor" /min cmd.exe /d /s /c "npm.cmd run gotrader:start >> .gotrader\gotrader-supervisor.log 2>&1"
-exit /b 0
