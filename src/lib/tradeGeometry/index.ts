c48a38d6b5848409a9376d23e256c038ab0caeae
Andre Duval <andre.duval@nationofduval.org>
2026-08-20 13:05:02 -0400

    feat(geometry): canonicalize risk reward and actionability

--- src/lib/tradeGeometry/index.ts
+++ src/lib/tradeGeometry/index.ts
@@ -1,4 +1,5 @@
 export * from "@/lib/tradeGeometry/tradeGeometryTypes";
 export * from "@/lib/tradeGeometry/entryLifecycle";
 export * from "@/lib/tradeGeometry/targetSelection";
+export * from "@/lib/tradeGeometry/canonicalTradeGeometry";
 
